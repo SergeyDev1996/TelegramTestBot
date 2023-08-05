420e0788ad4d83e8c3dacb87f773d369f6842252
Serhiy Zhyhadlo <sergeyz.dev1996@gmail.com>
2023-08-05 13:50:58 +0300

    Weather API ready

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
 
 
-TOKEN = os.environ.get("TELEGRAM_TOKEN")
+TOKEN = os.environ.get("TELEGRAM_TOKEN", None)
+WEATHER_API_KEY = os.environ.get("WEATHER_API_KEY", None)
--- main.py
+++ main.py
@@ -1,19 +1,25 @@
-"""
-This is a echo bot.
-It echoes any incoming text messages.
-"""
+import requests
+
 import logging
 
+import aiogram.utils.markdown as md
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Text
+from aiogram.dispatcher.filters.state import StatesGroup, State
+from aiogram.types import ParseMode
 
-from config import TOKEN
+
+from config import TOKEN, WEATHER_API_KEY
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 
 # Initialize bot and dispatcher
 bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
 
 
 @dp.message_handler(commands=['start', 'help'])
@@ -21,7 +27,76 @@ async def send_welcome(message: types.Message):
     """
     This handler will be called when user sends `/start` or `/help` command
     """
-    await bot.send_message(message.from_user.id, f"Welcome, {message.from_user.first_name}.")
+    first_name = message.from_user.first_name
+    await bot.send_message(message.from_user.id, f"Welcome, {first_name}.")
+
+
+# States
+class WeatherForm(StatesGroup):
+    city = State()  # Will be represented in storage as 'Form:gender'
+
+
+@dp.message_handler(commands='weather')
+async def cmd_start(message: types.Message):
+    """
+    Conversation's entry point
+    """
+    # Set state
+    await WeatherForm.city.set()
+
+    await message.reply("Hello! To get the weather, please enter your city name.")
+
+
+# You can use state '*' if you need to handle all states
+@dp.message_handler(state='*', commands='cancel')
+@dp.message_handler(Text(equals='cancel', ignore_case=True), state='*')
+async def cancel_handler(message: types.Message, state: FSMContext):
+    """
+    Allow user to cancel any action
+    """
+    current_state = await state.get_state()
+    if current_state is None:
+        return
+
+    logging.info('Cancelling state %r', current_state)
+    # Cancel state and inform user about it
+    await state.finish()
+    # And remove keyboard (just in case)
+    await message.reply('Cancelled.', reply_markup=types.ReplyKeyboardRemove())
+
+
+@dp.message_handler(state=WeatherForm.city)
+async def process_weather(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        city = message.text
+        url = 'http://api.openweathermap.org/data/2.5/weather'
+        params = {
+            'q': city,
+            'appid': WEATHER_API_KEY,
+            'units': 'metric',  # Use 'imperial' for Fahrenheit
+        }
+        response = requests.get(url, params=params)
+        if response.status_code == 200:
+            data = response.json()
+            weather_description = data['weather'][0]['description']
+            temp = data['main']['temp']
+            country = data['sys']['country']
+            response_text = f"Weather in {city}, {country}: {weather_description}, Temperature: {temp}°C"
+        else:
+            response_text = "Sorry, we were unable to process the weather for your city."
+        markup = types.ReplyKeyboardRemove()
+        # And send message
+        await bot.send_message(
+            message.chat.id,
+            md.text(
+                response_text
+            ),
+            reply_markup=markup,
+            parse_mode=ParseMode.MARKDOWN,
+        )
+    # Finish conversation
+    await state.finish()
+
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
